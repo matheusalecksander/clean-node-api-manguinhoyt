a14e3063cf5cd2dcde11c5de897887d289ee97ee
Matheus Alecksander <projetos.matheusalecksander@gmail.com>
2022-03-29 14:28:27 -0300

    modify HttpResponse.badRequest to receive an error

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -161,4 +161,18 @@ describe('Login Router', () => {
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
   })
+
+/*   it('Should return 400 if an invalid password is provided', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        email: 'invalid_email@email.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('password'))
+  }) */
 })
